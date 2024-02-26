903f9a71da7d2fd25c681887ff1e7c3551d82775
birukshegute <birukshegute@gmail.com>
2024-02-26 11:04:48 -0800

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,11 +10,14 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(self):
-    """ declaring teardown method to handle 
+    """ declaring teardown method to handle
     @app.teardown_appcontext that calls storage.close() """
     storage.close()
+
+
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST") is None:
         HBNB_API_HOST = '0.0.0.0'
