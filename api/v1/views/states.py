62952b712a53d43d181e10644f7d31b98b4ffbe3
birukshegute <birukshegute@gmail.com>
2024-02-26 14:27:27 -0800

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -18,7 +18,7 @@ def list_states():
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
-    '''Retrieves a State object'''
+    """Retrieves a State object"""
     states = storage.all("State").values()
     state_object = [obj.to_dict() for obj in states if obj.id == state_id]
     if state_object == []:
@@ -28,7 +28,7 @@ def get_state(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
-    '''Deletes a State object'''
+    """Deletes a State object"""
     states = storage.all("State").values()
     state_object = [obj.to_dict() for obj in states if obj.id == state_id]
     if state_object == []:
@@ -43,7 +43,7 @@ def delete_state(state_id):
 
 @app_views.route('/states/', methods=['POST'])
 def create_state():
-    '''Creates a State'''
+    """Creates a State"""
     if not request.get_json():
         abort(400, 'Not a JSON')
     if 'name' not in request.get_json():
@@ -58,7 +58,7 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def updates_state(state_id):
-    '''Updates a State object'''
+    """Updates a State object"""
     states = storage.all("State").values()
     state_object = [obj.to_dict() for obj in states if obj.id == state_id]
     if state_object == []:
