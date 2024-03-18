44a746a18a58787002e28de9cdec23f7c876c848
birukshegute <birukshegute@gmail.com>
2024-03-18 01:26:26 -0700

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Creating a new view for State objects"""
+"""
+Creating a new view for State objects
+"""
 
 from api.v1.views import app_views
 from flask import jsonify, abort, request
